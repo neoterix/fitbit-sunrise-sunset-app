98abb27edc2c265477b1b733be6424c84e2ed772
Anthony N <neoterix@users.noreply.github.com>
2019-04-19 17:21:28 -0400

    Added GPS caching to overcome delay

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import { preferences } from "user-settings";
 import * as util from "../common/utils";
 import { geolocation } from "geolocation";
 import { sunCalcLibrary } from "./sun";
+import * as fs from "fs";
 
 // Update the clock every minute
 clock.granularity = "minutes";
@@ -37,15 +38,108 @@ clock.ontick = (evt) => {
 
 // Sunrise/Sunset Calculation and Geolocation Code
 
-// I'm sorry this is a terrible hack
+// I'm so sorry this is a terrible hack to import library
 console.log("Confirm Library Import: " + sunCalcLibrary());
 sunCalcLibrary();
 
 const sunriseTimeText = document.getElementById("sunriseTimeText");
 const sunsetTimeText = document.getElementById("sunsetTimeText");
 
+// If initially waiting for GPS, usability messaging
+var gpswaitText = "Waiting for GPS Data";
+sunriseTimeText.text = `${gpswaitText}`;
+sunsetTimeText.text = `${gpswaitText}`;
+
+
+// Check to see if cache files are in place
+import { listDirSync } from "fs";
+function file_exists(filename) {
+    let dirIter = "";
+    let listDir = listDirSync("");
+    while((dirIter = listDir.next()) && !dirIter.done) if(dirIter.value===filename) return true;
+    return false;
+}
+
+if(file_exists("utf8_lat.txt")) {
+  console.log("GPS cache file exists, using cache");
+  let utf8_read_lat = parseFloat(fs.readFileSync("utf8_lat.txt", "utf-8"));
+  let utf8_read_long = parseFloat(fs.readFileSync("utf8_long.txt", "utf-8"));
+  let cacheSunriseTime = new Date().sunrise(utf8_read_lat, utf8_read_long);
+  let cacheSunsetTime = new Date().sunset(utf8_read_lat, utf8_read_long);
+  timeCalculation(cacheSunriseTime, cacheSunsetTime);
+} else {
+  console.log("GPS cache file does not exist");
+}
+
+//let file_stats = fs.existsSync("utf8_lat.txt");
+/*
+if (fs.access("utf8_lat.txt")) {
+  let utf8_read_lat = parseFloat(fs.readFileSync("utf8_lat.txt", "utf-8"));
+  let utf8_read_long = parseFloat(fs.readFileSync("utf8_long.txt", "utf-8"));
+  let cacheSunriseTime = new Date().sunrise(utf8_read_lat, utf8_read_long);
+  let cacheSunsetTime = new Date().sunset(utf8_read_lat, utf8_read_long);
+  timeCalculation(cacheSunriseTime, cacheSunsetTime);
+} else {
+  geolocation.getCurrentPosition(locationSuccess, locationError, getCurrentPosition_geo_options);
+}
+
+import { existsSync } from "fs";
+let chucktesta = existsSync("utf8_lat.txt");
+console.log("Sunrise:" + chucktesta);
+*/
+//import { openSync } from "fs";
+//openSync("utf8_lat.txt", "r");
+//console.log("contents:" + fs.listDirSync(""))
+
+/*
+import { listDirSync } from "fs";
+var listDir = listDirSync("/private/data");
+var dirIter;
+while((dirIter = listDir.next()) && !dirIter.done) {
+console.log("directory: " + dirIter.value);
+}
+*/
+
+
+function timeCalculation(sunriseTime, sunsetTime) {
+  let sunriseTimeHours = sunriseTime.getHours();
+  let sunriseTimeMins = util.zeroPad(sunriseTime.getMinutes());
+  let sunsetTimeHours = sunsetTime.getHours() % 12 || 12;
+  let sunsetTimeMins = util.zeroPad(sunsetTime.getMinutes());
+  console.log("Sunrise:" + sunriseTime);
+  console.log("Sunset:" + sunsetTime);
+  if (preferences.clockDisplay === "12h") {
+    sunriseTimeHours = sunriseTimeHours % 12 || 12;
+    sunsetTimeHours = sunsetTimeHours % 12 || 12;
+    sunriseTimeText.text = `${sunriseTimeHours}:${sunriseTimeMins} AM`;
+    sunsetTimeText.text = `${sunsetTimeHours}:${sunsetTimeMins} PM`;
+  } else {
+    sunriseTimeHours = util.zeroPad(sunriseTimeHours);
+    sunsetTimeHours = util.zeroPad(sunsetTimeHours);
+    sunriseTimeText.text = `${sunriseTimeHours}:${sunriseTimeMins}`;
+    sunsetTimeText.text = `${sunsetTimeHours}:${sunsetTimeMins}`;
+  }
+}
+
+
+
 function locationSuccess(position) {
+  //Caching new GPS coords for future access
+  let utf8_lat = position.coords.latitude.toString();
+  let utf8_long = position.coords.longitude.toString();
+  fs.writeFileSync("utf8_lat.txt", utf8_lat, "utf-8");
+  fs.writeFileSync("utf8_long.txt", utf8_long, "utf-8");
+  //debugging
+  //let utf8_read_lat = fs.readFileSync("utf8_lat.txt", "utf-8");
+  //console.log("UTF-8 Lat Data: " + utf8_read_lat);
+  //let utf8_read_long = fs.readFileSync("utf8_long.txt", "utf-8");
+  //console.log("UTF-8 Long Data: " + utf8_read_long);
+  let gpsSunriseTime = new Date().sunrise(position.coords.latitude, position.coords.longitude);
+  let gpsSunsetTime = new Date().sunset(position.coords.latitude, position.coords.longitude);
+  timeCalculation(gpsSunriseTime, gpsSunsetTime);
+}
   
+  /*
   //Sunrise
   var sunriseTime = new Date().sunrise(position.coords.latitude, position.coords.longitude);
   let sunriseTimeHours = sunriseTime.getHours();
@@ -71,27 +165,22 @@ function locationSuccess(position) {
     sunsetTimeHours = util.zeroPad(sunsetTimeHours);
     sunsetTimeText.text = `${sunsetTimeHours}:${sunsetTimeMins}`;
   }
-}
+
+
+*/
 
 function locationError(error) {
   console.log("Error: " + error.code,
               "Message: " + error.message);
-  let errorText = "No GPS, Please Wait";
+  let errorText = "No GPS, Try Again";
   sunriseTimeText.text = `${errorText}`;
   sunsetTimeText.text = `${errorText}`;
 }
 
-var getCurrentPosition_geo_options = {
+var geo_options = {
   enableHighAccuracy: false, 
   maximumAge        : 0, 
   timeout           : Infinity,
 };
 
-var watchPosition_geo_options = {
-  enableHighAccuracy: false, 
-  maximumAge        : 0, 
-  timeout           : 1000,
-};
-
-geolocation.getCurrentPosition(locationSuccess, locationError, getCurrentPosition_geo_options);
-//geolocation.watchPosition(locationSuccess, locationError, watchPosition_geo_options);
+geolocation.getCurrentPosition(locationSuccess, locationError, geo_options);
